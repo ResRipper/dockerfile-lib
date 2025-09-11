0b33b84507ed2201300d092899c76f45dd9a153c
ResRipper <resripper@connective.link>
2025-09-11 13:40:59 +0800

    Update run.sh
    - Change data folders' ownership during start-up
    - Run services under `www-data` user with `su`
    - Copy data to mounted folder if its empty

--- content/grav/run.sh
+++ content/grav/run.sh
@@ -2,8 +2,25 @@
 # Copyright 2025 resripper.
 # SPDX-License-Identifier: MIT
 
+# Change folders' owner
+if [ "${CHOWN:=true}" = true ]; then
+    echo "Adjusting data folders' ownership..."
+    chown -R www-data:www-data /var/www/html/user /var/www/html/backup /var/www/html/logs
+fi
+
 # Schedule maintenance tasks
-supercronic -quiet /etc/crontabs/www-data &
+su -s /bin/sh www-data -c "supercronic -quiet /etc/crontabs/www-data &"
+
+# Transfer user files if mountpoint is empty
+su -s /bin/sh www-data -c "$(cat << EOF
+    if [ -z "$( ls -A '/var/www/html/user/' )" ]; then
+        echo 'User data empty, copying files...'
+        cp -r /var/www/user_resources/* /var/www/html/user/
+    else
+        echo 'Files detected, skip copying.'
+    fi
+EOF
+)"
 
 # Start Grav service
-php-fpm -D && caddy run -c /etc/caddy/Caddyfile
+su -s /bin/sh www-data -c "php-fpm -D && caddy run -c /etc/caddy/Caddyfile"
